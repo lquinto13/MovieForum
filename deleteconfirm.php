c4cf4c8c30c448bc17365bdeabc3202a31a2dac2
riantheworld <60253040+riantheworld@users.noreply.github.com>
2021-12-04 14:38:58 +0800

    Update deleteconfirm.php

--- deleteconfirm.php
+++ deleteconfirm.php
@@ -18,6 +18,15 @@ include 'header.php'
 <head>
     <title>Home</title>
     <link rel="stylesheet" type="text/css">
+    <!-- Bootstrap -->
+	<link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+	<!-- MDB Style -->
+	<link rel="stylesheet" href="mdb-bootstrap-3.10.1/css/mdb.min.css" />
+	<!-- Font Awesome -->
+	<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.15.2/css/all.css" />
+	<!-- Custom Styles -->
+	<link rel="stylesheet" type="text/css" href="css/style.css">
+	<link rel="stylesheet" type="text/css" href="css/table.css">
 </head>
 <style>
     .center {
@@ -101,7 +110,7 @@ include 'header.php'
                     $sql = "DELETE FROM categories WHERE cat_id = $id";
                     if (mysqli_query($db, $sql)) {
                         echo "<div class='center'>";
-                        echo "<p>You have deleted the category succesfully.</p>";
+                        echo "<div class="alert alert-primary" role="alert"><p>You have deleted the category succesfully.</p></div>";
                         echo "<div class='content-container'>";
                         echo 'Return to  <a href="index.php?id=) ">Home Page</a>.';
                         echo "</div>";
@@ -117,7 +126,14 @@ include 'header.php'
 
         ?>
     </div>
-
+<!-- JQuery -->
+<script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+	<!-- Popper JS -->
+	<script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.7/umd/popper.min.js" integrity="sha384-UO2eT0CpHqdSJQ6hJty5KVphtPhzWj9WO1clHTMGa3JDZwrnQq4sF86dIHNDz0W1" crossorigin="anonymous"></script>
+	<!-- Bootstrap Script -->
+	<script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+	<!-- MDB Script -->
+	<script type="text/javascript" src="mdb-bootstrap-3.10.1/js/mdb.min.js"></script>
 </body>
 
 </html>
